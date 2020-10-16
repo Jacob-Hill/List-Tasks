cf026828e674856d307ba5c3d259e15fae064f64
Jacob <hillj@stpaulsschool.org.uk>
2020-10-16 12:37:57 +0100

    Remade tests and created the IsListSubset function.

--- List Tasks/Program.cs
+++ List Tasks/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 
@@ -11,26 +12,37 @@ namespace List_Tasks
         {
             List<int> testListInt = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             List<int> testListNegativeInt = new List<int> { -9, -8, -7, -6, -5, -4, -3, -2, -1, 0 };
-            Debug.Assert(SumList(testListInt) == testListInt.Sum());
-            Console.WriteLine("Test Passed");
-            Debug.Assert(MaxList(testListInt) == testListInt.Max());
-            Console.WriteLine("Test Passed");
-            Debug.Assert(MinList(testListInt) == testListInt.Min());
-            Console.WriteLine("Test Passed");
-            Debug.Assert(ListsContainSameInts(NegativeList(testListNegativeInt), new List<int> { -9, -8, -7, -6, -5, -4, -3, -2, -1 }));
-            Console.WriteLine("Test Passed");
-            Debug.Assert(ListsContainSameInts(testListInt, testListInt));
-            Console.WriteLine("Test Passed");
-            Debug.Assert(ListsContainSameInts(testListInt, new List<int> { 1, 1, 2, 3, 4, 5, 6, 7, 4, 6, 4, 8, 9, 1 }));
-            Console.WriteLine("Test Passed");
-            Debug.Assert(SimpleListSearchInt(testListInt, 5));
-            Console.WriteLine("Test Passed");
-            Debug.Assert(SimpleListSearchInt(testListInt, 10) == false);
-            Console.WriteLine("Test Passed");
-            Debug.Assert(BinaryListSearchInt(testListInt, 5));
-            Console.WriteLine("Test Passed");
-            Debug.Assert(BinaryListSearchInt(testListInt, 10) == false);
-            Console.WriteLine("Test Passed");
+            try
+            {
+                if(SumList(testListInt) == testListInt.Sum()) { Console.WriteLine("SumList Test Passed"); }
+                else { throw new Exception("SumList Test Failed"); }
+                if(MaxList(testListInt) == testListInt.Max()) { Console.WriteLine("MaxList Test Passed"); }
+                else { throw new Exception("MaxList Test Failed"); }
+                if(MinList(testListInt) == testListInt.Min()) { Console.WriteLine("MinList Test Passed"); }
+                else { throw new Exception("MinList Test Failed"); }
+                if(ListsContainSameInts(NegativeList(testListNegativeInt), new List<int> { -9, -8, -7, -6, -5, -4, -3, -2, -1 })) { Console.WriteLine("NegativeList Test Passed"); }
+                else { throw new Exception("NegativeList Test Failed"); }
+                if(ListsContainSameInts(testListInt, testListInt)) { Console.WriteLine("ListsContainSameInts Test Passed"); }
+                else { throw new Exception("ListsContainSameInts Test 1 Failed"); }
+                if(ListsContainSameInts(testListInt, new List<int> { 1, 1, 2, 3, 4, 5, 6, 7, 4, 6, 4, 8, 9, 1 })) { Console.WriteLine("ListsContainSameInts Test Passed"); }
+                else { throw new Exception("ListsContainSameInts Test 2 Failed"); }
+                if(SimpleListSearchInt(testListInt, 5)) { Console.WriteLine("SimpleListSearch Test Passed"); }
+                else { throw new Exception("SimpleListSearch Test 1 Failed"); }
+                if(SimpleListSearchInt(testListInt, 10) == false) { Console.WriteLine("SimpleListSearch Test Passed"); }
+                else { throw new Exception("SimpleListSearch Test 2 Failed"); }
+                if(BinaryListSearchInt(testListInt, 5)) { Console.WriteLine("BinaryListSearchInt Test Passed"); }
+                else { throw new Exception("BinaryListSearchInt Test 1 Failed"); }
+                if(BinaryListSearchInt(testListInt, 10) == false) { Console.WriteLine("BinaryListSearchInt Test Passed"); }
+                else { throw new Exception("BinaryListSearchInt Test 1 Failed"); }
+                if(ListsContainSameInts(DuplicateIntsList(new List<int> { 1, 1, 2, 1, 3, 6, 2, 8, 6, 4, 2, 9, 8, 9 }), new List<int> { 1, 2, 6, 8, 9 })) { Console.WriteLine("DuplicateIntsList Test Passed"); }
+                else { throw new Exception("DuplicateIntsList Test 1 Failed"); }
+                if (IsListSubset(testListInt, new List<int> { 1, 2, 3 })) { Console.WriteLine("IsListSubset Test Passed"); }
+                else { throw new Exception("IsListSubset Test Failed"); }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
 
@@ -139,6 +151,45 @@ namespace List_Tasks
                 }
             }
             return false;
+        }
+
+        static List<int> DuplicateIntsList(List<int> list)
+        {
+            List<int> result = new List<int> { };
+            List<int> intsSoFar = new List<int> { };
+            for(int i = 0; i<list.Count; i++)
+            {
+                if (intsSoFar.Contains(list[i]))
+                {
+                    if(!(result.Contains(list[i])))
+                    {
+                        result.Add(list[i]);
+                    }
+                }
+                else
+                {
+                    intsSoFar.Add(list[i]);
+                }
+            }
+            return result;
+        }
+
+        static bool IsListSubset(List<int> list1, List<int> list2)
+        {
+            if (list2.Count > list1.Count)
+            {
+                List<int> list3 = list1;
+                list1 = list2;
+                list2 = list3;
+            }
+            for (int i = 0; i < list2.Count; i++)
+            {
+                if (!list1.Contains(list2[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
